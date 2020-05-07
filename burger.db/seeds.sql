4e8d6124e1d1c8e4e56ea6d9c967390d11403462
Drapeto <drapetorres@gmail.com>
2020-05-06 19:00:33 -0700

    App and database are setup

---
+++ burger.db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheese burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('el diablo', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('avacado burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('happy burger', false);
---
+++ server.js
@@ -0,0 +1,9 @@
+var exphbs = require("express-handlebars");
+var express = require("express");
+var mysql = require("mysql");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
